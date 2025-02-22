67975b67f5fb007dfe04f7229bb10e88f5316eb5
shellaeffertz <covowakafoj66@gmail.com>
2025-02-22 18:31:18 +0100

    add logo

--- src/components/Cybernest.vue
+++ src/components/Cybernest.vue
@@ -1,25 +1,5 @@
 <template>
     <div class="flex-shrink-0">
-        <h1 class="logo-text text-2xl font-extrabold">
-            <span class="text-blue-500">Cyber</span><span class="text-gray-800">nest</span>
-            <span class="logo-dot text-blue-500">.</span>
-        </h1>
+        <img src="/images/cybernest.png" alt="Cybernest Logo" class="w-32 sm:w-44">
     </div>
 </template>
-
-<style scoped>
-
-.logo-text {
-  letter-spacing: -1px;
-  position: relative;
-}
-
-.logo-dot {
-  display: inline-block;
-  font-size: 2.5rem;
-  line-height: 0;
-  position: relative;
-  top: -2px;
-}
-
-</style>
